84645677f454f7967c0169d25d1de588bbadc30d
Baranuel <samuel.baran98@gmail.com>
2023-03-17 17:44:53 +0100

    typo

--- fetch-data.ts
+++ fetch-data.ts
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 
-const env = dev ? "http://localhost:3000" : "https://witch-quizz.vercel.app/";
+const env = dev ? "http://localhost:3000" : "https://witch-quizz.vercel.app";
 
 export const getQuestions = async () => {
   const data = await fetch(`${env}/api/questions`);
