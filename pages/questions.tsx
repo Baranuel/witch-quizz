9d8b85e41d72c1222e364fcae038a3811845a0d8
Samuel Baran <samuel.baran98@gmail.com>
2023-03-26 11:56:58 +0200

    hack ?

--- pages/questions.tsx
+++ pages/questions.tsx
@@ -5,7 +5,11 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import Button from "../components/Button";
 import DialogueBox from "../components/DialogueBox";
 import { getQuestions } from "../fetch-data";
-import { answersAnimate, textBubbleAnimate, variants } from "../globals/animations";
+import {
+  answersAnimate,
+  textBubbleAnimate,
+  variants,
+} from "../globals/animations";
 import { introduction } from "../globals/introduction";
 import { responses_after_wrong_answer } from "../globals/responses";
 import { Head } from "next/document";
@@ -20,7 +24,7 @@ interface questionDto {
 }
 
 interface DialogueBubbleDto {
-  question?: boolean ;
+  question?: boolean;
   src: string;
   text: string;
   clue?: string;
@@ -29,69 +33,76 @@ interface DialogueBubbleDto {
 function Questions({
   _questions,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-
-  const story:questionDto[] = introduction.concat(_questions)
+  const story: questionDto[] = introduction.concat(_questions);
   const [firstRender, setFirstRender] = useState(true);
   const [numberOfAddedBubbles, setNumberOfAddedBubbles] = useState(1);
   const [tryGuessing, setTryGuessing] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState<questionDto>(story[0]);
-  const [arrayOfWrongResponses, setArrayOfWrongResponses] = useState(responses_after_wrong_answer)
-  const [dialogueBox, setDialogueBox] = useState<DialogueBubbleDto[]>(currentQuestion.story);
+  const [arrayOfWrongResponses, setArrayOfWrongResponses] = useState(
+    responses_after_wrong_answer
+  );
+  const [dialogueBox, setDialogueBox] = useState<DialogueBubbleDto[]>(
+    currentQuestion.story
+  );
 
-  const isLastQuestion = currentQuestion.question_number === story[story.length - 1].question_number
+  const isLastQuestion =
+    currentQuestion.question_number === story[story.length - 1].question_number;
   const [revealClues, setRevealClues] = useState(isLastQuestion && tryGuessing);
   const appRef = useRef<HTMLDivElement>(null!);
+
   useEffect(() => {
-   if(!appRef.current) return;
-    appRef.current.scrollIntoView({behavior: "smooth"})
-  }, [])
-
-  useEffect( () => {
-    if(!firstRender) {
-      const {question} = currentQuestion
-      updateDialogueBoxInSequence(currentQuestion.story, question)
-    } else {
-      setFirstRender(false)
-    }
-  },[currentQuestion])
-
+    if (!appRef.current) return;
+    appRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
   
 
+  useEffect(() => {
+    if (!firstRender) {
+      const { question } = currentQuestion;
+      updateDialogueBoxInSequence(currentQuestion.story, question);
+    } else {
+      setFirstRender(false);
+    }
+  }, [currentQuestion]);
+
   const updateCurrentQuestion = () => {
-    if(isLastQuestion) return;
+    if (isLastQuestion) return;
     setCurrentQuestion((prev) => {
       return {
         ...story[prev.question_number + 1],
-      }
+      };
     });
-    
   };
 
   const updateDialogueBoxWithAnswer = (answer: string) => {
-    setNumberOfAddedBubbles(1)
-    setDialogueBox((prev:any) => [
-      ...prev,
-      {  src: "Me", text: answer },
-    ]);
+    setNumberOfAddedBubbles(1);
+    setDialogueBox((prev: any) => [...prev, { src: "Me", text: answer }]);
   };
 
   const updateDialogueAfterWrongAnswer = () => {
-    const randomResponse = arrayOfWrongResponses[Math.floor(Math.random() * arrayOfWrongResponses.length)]
+    const randomResponse =
+      arrayOfWrongResponses[
+        Math.floor(Math.random() * arrayOfWrongResponses.length)
+      ];
 
     //testing if we can update in sequence
-    const text = [{ src: "Rhysand", text: randomResponse}]
-    updateDialogueBoxInSequence(text)
-    setArrayOfWrongResponses((prev) => prev.filter((response) => response !== randomResponse))
-   }
+    const text = [{ src: "Rhysand", text: randomResponse }];
+    updateDialogueBoxInSequence(text);
+    setArrayOfWrongResponses((prev) =>
+      prev.filter((response) => response !== randomResponse)
+    );
+  };
 
-   const updateDialogueBoxInSequence = (bubbles:DialogueBubbleDto[], question?:DialogueBubbleDto) => {
-      const arrayOfStoryAndQuestion = question ? bubbles.concat(question) : bubbles
-      setNumberOfAddedBubbles((prev) => arrayOfStoryAndQuestion.length);
-      setDialogueBox((prev:any) => [
-        ...prev.concat(arrayOfStoryAndQuestion)
-      ]);
-   }
-  
+  const updateDialogueBoxInSequence = (
+    bubbles: DialogueBubbleDto[],
+    question?: DialogueBubbleDto
+  ) => {
+    const arrayOfStoryAndQuestion = question
+      ? bubbles.concat(question)
+      : bubbles;
+    setNumberOfAddedBubbles((prev) => arrayOfStoryAndQuestion.length);
+    setDialogueBox((prev: any) => [...prev.concat(arrayOfStoryAndQuestion)]);
+  };
 
   const determineCorrectAnswer = (answer: string) => {
     if (answer === currentQuestion.correct_answer) {
@@ -99,8 +110,8 @@ function Questions({
       updateCurrentQuestion();
     } else {
       updateDialogueAfterWrongAnswer();
+    }
   };
-}
 
   const handleAnswer = (answer: string) => {
     updateDialogueBoxWithAnswer(answer);
@@ -109,77 +120,103 @@ function Questions({
     }, 750);
   };
 
-
   return (
     <>
-    
-    
-  <motion.div ref={appRef} className="flex flex-col justify-between h-screen">
-    <LayoutGroup>
-
-      <motion.div className="bg-bg-primary h-screen w-screen flex flex-col items-start justify-between">
-        <motion.div
-          layout
-          className="mt-2"
-          >
-          <motion.h1
-          onClick={() => setTryGuessing(prev => !prev)}
-          layout
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.25 }}
-          className="text-[calc(100vw/10)] mb-4 px-4 font-k2d text-color-heading "
-          >
-          Encounter
-        </motion.h1>
-          <DialogueBox isLastQuestion={isLastQuestion} numberOfAddedBubbles={numberOfAddedBubbles} dialogue={dialogueBox} setTryGuessing={setTryGuessing} revealClues={revealClues} setRevealClues={setRevealClues} />
-        </motion.div>
-
-  <AnimatePresence initial={false} mode='popLayout'>
-   {!isLastQuestion &&
-    <motion.ul  variants={variants} initial="hidden" animate='show' exit={{opacity: 0,scale:0.5, y:50}} className="grid grid-cols-2 w-full p-2 
-    h-[120px] items-center gap-2">
-        <AnimatePresence initial={false} mode='popLayout'>
-
-           {tryGuessing &&
-             currentQuestion.possible_answers.map(
-               (answer: string, index: number) => {
-                 return (
-                   <motion.li variants={answersAnimate} exit={{opacity: 0,scale:0.5, y:50}}   transition={{ duration: 0.3 }}
-                      key={index} className='w-full'>
-                     <Button
-                       text={answer}
-                       onClick={() => {
-                         handleAnswer(answer);
-                       }}
-                       />
-                   </motion.li>)})
-                   
-                  }
-                  </AnimatePresence>
-         </motion.ul> }
-         </AnimatePresence>
-         
-         <AnimatePresence initial={false} mode='popLayout'>
-          {isLastQuestion && 
-         <motion.div variants={variants} initial="hidden" animate='show' exit={{opacity: 0,scale:0.5, y:50}} className="flex h-[100px] items-center p-2 w-full gap-2 justify-between">
-            <AnimatePresence initial={false} mode='popLayout'>
-          {tryGuessing &&
-            <motion.div variants={answersAnimate} exit={{opacity: 0,scale:0.5, y:50}} className=" flex items-center w-full gap-2 justify-between">
-            <input type="text" className="bg-bg-secondary text-color-heading p-3 w-full basis-2/3 rounded-md bg-custom-black/30 border border-color-primary "/>
-            <div className="w-full basis-1/3">
-            <Button text="Cast" onClick={() => {}} />
-            </div>
+      <motion.div
+        ref={appRef}
+        className="flex flex-col justify-between h-screen fixed top-0"
+      >
+        <LayoutGroup>
+          <motion.div className="bg-bg-primary h-screen w-screen flex flex-col items-start justify-between">
+            <motion.div layout className="mt-2">
+              <motion.h1
+                onClick={() => setTryGuessing((prev) => !prev)}
+                layout
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.25 }}
+                className="text-[calc(100vw/10)] mb-4 px-4 font-k2d text-color-heading "
+              >
+                Encounter
+              </motion.h1>
+              <DialogueBox
+                isLastQuestion={isLastQuestion}
+                numberOfAddedBubbles={numberOfAddedBubbles}
+                dialogue={dialogueBox}
+                setTryGuessing={setTryGuessing}
+                revealClues={revealClues}
+                setRevealClues={setRevealClues}
+              />
             </motion.div>
-            }
+
+            <AnimatePresence initial={false} mode="popLayout">
+              {!isLastQuestion && (
+                <motion.ul
+                  variants={variants}
+                  initial="hidden"
+                  animate="show"
+                  exit={{ opacity: 0, scale: 0.5, y: 50 }}
+                  className="grid grid-cols-2 w-full p-2 h-[120px] items-center gap-2"
+                >
+                  <AnimatePresence initial={false} mode="popLayout">
+                    {tryGuessing &&
+                      currentQuestion.possible_answers.map(
+                        (answer: string, index: number) => {
+                          return (
+                            <motion.li
+                              variants={answersAnimate}
+                              exit={{ opacity: 0, scale: 0.5, y: 50 }}
+                              transition={{ duration: 0.3 }}
+                              key={index}
+                              className="w-full"
+                            >
+                              <Button
+                                text={answer}
+                                onClick={() => {
+                                  handleAnswer(answer);
+                                }}
+                              />
+                            </motion.li>
+                          );
+                        }
+                      )}
+                  </AnimatePresence>
+                </motion.ul>
+              )}
             </AnimatePresence>
-            
+
+            <AnimatePresence initial={false} mode="popLayout">
+              {isLastQuestion && (
+                <motion.div
+                  variants={variants}
+                  initial="hidden"
+                  animate="show"
+                  exit={{ opacity: 0, scale: 0.5, y: 50 }}
+                  className="flex h-[100px] items-center p-2 w-full gap-2 justify-between"
+                >
+                  <AnimatePresence initial={false} mode="popLayout">
+                    {tryGuessing && (
+                      <motion.div
+                        variants={answersAnimate}
+                        exit={{ opacity: 0, scale: 0.5, y: 50 }}
+                        className=" flex items-center w-full gap-2 justify-between"
+                      >
+                        <input
+                          type="text"
+                          className="bg-bg-secondary text-color-heading p-3 w-full basis-2/3 rounded-md bg-custom-black/30 border border-color-primary "
+                        />
+                        <div className="w-full basis-1/3">
+                          <Button text="Cast" onClick={() => {}} />
+                        </div>
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </motion.div>
-         }
-         </AnimatePresence>
+        </LayoutGroup>
       </motion.div>
-    </LayoutGroup>
-    </motion.div>
     </>
   );
 }
